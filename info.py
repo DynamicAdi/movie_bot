387e6b9940f6db68e84948c9e5244caf85d4a621
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2022-05-19 11:55:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -8,7 +8,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
 API_ID = 2698821
 API_HASH = "4af625c75eb9e72e1b228411b0c7cd42"
-BOT_TOKEN = ""
+BOT_TOKEN = "5339403812:AAFOcjd06QmGw6PaF5HEOxuzhf6h9ZiZ5UU"
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
 # Bot settings
@@ -16,16 +16,16 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = 
-CHANNELS = ""
+ADMINS = [819445076, 1366616835]
+CHANNELS = "-1001500424852"
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI'] ""
-DATABASE_NAME = environ['DATABASE_NAME']
+DATABASE_URI = ""
+DATABASE_NAME = " "
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
