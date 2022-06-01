74fda31d048493c01f8747d018e786f81f436ed9
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2022-06-01 16:56:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,5 @@
 import re
+
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
@@ -6,9 +7,9 @@ id_pattern = re.compile(r'^.\d+$')
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = 2698821
-API_HASH = "4af625c75eb9e72e1b228411b0c7cd42"
-BOT_TOKEN = "5339403812:AAFOcjd06QmGw6PaF5HEOxuzhf6h9ZiZ5UU"
+API_ID = environ.get("API_ID", "")
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", ""
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
 # Bot settings
@@ -16,16 +17,16 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = [819445076, 1366616835]
-CHANNELS = "-1001500424852"
+ADMINS = environ.get('ADMINS', '')
+CHANNELS = environ.get("CHANNELS", "")
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://RobinDB:RobinHood@cluster0.yk7s6.mongodb.net/?retryWrites=true&w=majority"
-DATABASE_NAME = "RobinDB"
+DATABASE_URI = environ.get("DATABASE_URI", "")
+DATABASE_NAME = environ.get("DATABASE_NAME", "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
